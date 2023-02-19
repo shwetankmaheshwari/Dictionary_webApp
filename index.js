2d1c1b83aa185559582aa341ee642fd6c455f3f0
SHWETANK MAHESHWARI <shwetank.maheshwari.5@gmail.com>
2023-02-19 13:17:16 +0530

    second version

--- index.js
+++ index.js
@@ -26,89 +26,110 @@ mongoose.connect(mongoUrl, (err) => {
 app.get("/", function (req, res) {
   res.render("home", { results: [], searchTerm: "" });
 });
+
 app.post("/", function (req, res) {
   const searchTerm = req.body.searchTerm;
+  
   DictionarySchema.find(
     { Word: { $regex: searchTerm, $options: "i" } },
     (error, results) => {
       if (error) throw error;
-      res.render("home", { results: results, searchTerm: searchTerm });
+     if(req.body.action === 'delete-update'){
+      
+      res.render('update',{ results: results, searchTerm: searchTerm });
+     }
+     
+     else res.render("home", { results: results, searchTerm: searchTerm });
     }
   );
 });
 
+// app.patch('/update',(req,res)=>{
+//   console.log('find')
+
+//   const action = req.body.action;
+//   console.log(action);
+//    if (action === "delete-update") {
+     
+//     const find_word = req.body.find_word;
+//     DictionarySchema.find({ Word: { $regex: find_word, $options: "i" } },
+//       (error, result) => {
+//         if (error) throw error;
+       
+//         res.render("update", { results: result, find_word: find_word });
+//       }
+//     );
+//   }
+// })
 app.get("/update", (req, res) => {
+  
   res.render("update",{ results: [], find_word: '' });
 });
 
 app.post("/update", (req, res) => {
   const action = req.body.action;
-
+   
   if (action === "insert") {
     const word = new DictionarySchema({
       Word: req.body.word,
       Definition: req.body.definition,
     });
     word.save();
-    res.sendStatus(200);
-  } else if (action === "delete-update") {
-    const find_word = req.body.find_word;
-    DictionarySchema.find({ Word: { $regex: find_word, $options: "i" } },
-      (error, result) => {
-        if (error) throw error;
-       
-        res.render("update", { results: result, find_word: find_word });
-      }
-    );
-  } else {
-    const type = req.body.action.type;
-    const id = req.body.action.id;
-    if (type === "delete") {
-      DictionarySchema.findByIdAndDelete({ _id: id }, (err) => {
+    res.redirect("/update");
+  } 
+  
+});
+
+app.post('/delete',(req,res)=>{
+  
+    const id = req.body.action;
+      DictionarySchema.findByIdAndDelete( id , (err,doc) => {
         if (err) throw err;
         else {
-          console.log("element deleted");
-          res.sendStatus(200);
+          console.log("element deleted" , doc);
+          res.json(doc);
         }
       });
-    } else if (type === "update") {
-      const word = req.body.word;
-      const define = req.body.definition;
-      if (word != "" && define != "") {
-        DictionarySchema.findByIdAndUpdate(
-          id,
-          { Word: word, Definition: define },
-          (err) => {
-            if (err) throw err;
-            else {
-              console.log("element is updated");
-              res.sendStatus(200);
-            }
-          }
-        );
-      } else if (word == "") {
-        DictionarySchema.findByIdAndUpdate(
-          id,
-          { Definition: define },
-          (err) => {
-            if (err) throw err;
-            else {
-              console.log("element is updated");
-              res.sendStatus(200);
-            }
-          }
-        );
-      } else if (define == "") {
-        DictionarySchema.findByIdAndUpdate(id, { Word: word }, (err) => {
+    });
+
+app.post('/updateElement',(req,res)=>{
+
+  const id = req.body.action;
+  
+    const word = req.body.word;
+    const define = req.body.definition;
+    if (word != "" && define != "") {
+      DictionarySchema.findByIdAndUpdate(
+        id,
+        { Word: word, Definition: define },
+        (err) => {
           if (err) throw err;
           else {
             console.log("element is updated");
-            res.sendStatus(200);
+            res.redirect('/update');
           }
-        });
-      }
+        }
+      );
+    } else if (word == "") {
+      DictionarySchema.findByIdAndUpdate(
+        id,
+        { Definition: define },
+        (err) => {
+          if (err) throw err;
+          else {
+            console.log("element is updated");
+            res.redirect('/update');
+          }
+        }
+      );
+    } else if (define == "") {
+      DictionarySchema.findByIdAndUpdate(id, { Word: word }, (err) => {
+        if (err) throw err;
+        else {
+          console.log("element is updated");
+          res.redirect('/update');
+        }
+      });
     }
   }
-
-  res.redirect("/update");
-});
+)
